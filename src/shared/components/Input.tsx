d29987043e4507676b0bfc3f1733166dd7d56388
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2025-10-24 10:02:37 +0700

    feat(hiring-platform): :art: update input component

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,14 +1,26 @@
+import classNames from "classnames";
+
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
+    errorMessage?: string;
+    disabled?: boolean;
 }
 
 export const Input: React.FC<InputProps> = (props) => {
+    const className = classNames({
+        'border-red-500': props.errorMessage,
+        'bg-neutral-30 cursor-not-allowed': props.disabled,
+    },
+        `w-full text-m border-2 border-neutral-40 rounded-md py-2 px-4 outline-none hover:border-primary-focus focus:border-primary transition-colors duration-200`,
+        props.className
+    )
+
     return (
         <div className="flex flex-col items-start">
-            {props.label && <label className="mb-2 text-m font-semibold">{props.label}</label>}
+            {props.label && <label className="mb-2 text-s font-regular">{props.label}</label>}
             <input
                 {...props}
-                className={`w-full text-m border-2 border-neutral-40 rounded-md py-2 px-4 outline-none focus:border-primary active:border-primary transition-colors duration-200 ${props.className}`}
+                className={className}
             />
         </div>
     );
